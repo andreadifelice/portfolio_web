0532a26f1888427ea4d68553a11a143b557bdaa5
adf_creations <andreadifelice54@gmail.com>
2026-06-28 12:39:37 +0200

    general fixes

--- components/ui/scroll-animation.tsx
+++ components/ui/scroll-animation.tsx
@@ -7,6 +7,9 @@ import { HeroSection } from './HeroSection';
 import { siGithub, siInstagram } from 'simple-icons';
 import ChangelogContent from '../shadcn-studio/blocks/timeline-component-05/timeline-component-05';
 import { useIsMobile } from '@/lib/use-mobile';
+import { MagicCard } from './magic-card';
+import Link from 'next/link';
+import { Button } from './button';
 
 const heroProps = {
     mainText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultrices, justo vel tempus.",
@@ -23,12 +26,40 @@ const heroProps = {
     ],
 };
 
+const projectProps = [
+    {
+        title: 'Progetto 1',
+        description: 'Descrizione progetto 1',
+        link: '/progetti'
+    },
+    {
+        title: 'Progetto 2',
+        description: 'Descrizione progetto 2',
+        link: '/progetti'
+    },
+]
+
 function ProjectsSection() {
     return (
-        <div className='container relative z-10 mx-auto flex flex-col gap-20 p-8'>
-        <p className='text-xl font-semibold tracking-tight sm:text-2xl md:text-3xl'>
-            I miei progetti
-        </p>
+        <div className='container relative z-10 mx-auto flex flex-col gap-20 p-8 pt-25'>
+        <div className='flex flex-col gap-5'>
+            <p className='text-xl font-semibold tracking-tight sm:text-2xl md:text-3xl'>
+                I miei progetti
+            </p>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-2.5'>
+                {projectProps.map((project, i) => (
+                    <Link href={project.link}>
+                        <MagicCard className='rounded-lg p-4'>
+                            <div className='flex flex-col gap-2'>
+                                <p>{project.title}</p>
+                                <span>{project.description}</span>
+                                <Button>Guarda su github</Button>
+                            </div>
+                        </MagicCard>
+                    </Link>
+                ))}
+            </div>
+        </div>
         <ChangelogContent />
         </div>
     );
@@ -57,8 +88,8 @@ const Section2 = ({ scrollYProgress }) => {
         style={{ scale, rotate }}
         className='relative h-fit bg-background text-primary'
         >
-        <div className='absolute inset-0 grid-pattern-lines mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]' />
-        <ProjectsSection />
+            <div className='absolute inset-0 grid-pattern-lines mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]' />
+            <ProjectsSection />
         </motion.section>
     );
 };
@@ -86,8 +117,8 @@ function DesktopScroll() {
 
     return (
         <main ref={container} className='relative h-[200vh] bg-black'>
-        <Section1 scrollYProgress={scrollYProgress} />
-        <Section2 scrollYProgress={scrollYProgress} />
+            <Section1 scrollYProgress={scrollYProgress} />
+            <Section2 scrollYProgress={scrollYProgress} />
         </main>
     );
 }
